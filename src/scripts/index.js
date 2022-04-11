4fa97038b4a81c86f888e2f44b7c86d31f26897b
M. Fajar Aditya <mfajaraditya13@gmail.com>
2022-04-11 11:13:18 +0800

    get API from DATA.json, create skip to content.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,35 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import img from '../public/images/heros/hero.jpg';
+
+import ('../DATA.json').then(({default: jsonData}) => {
+    console.log(jsonData)
+    let datas = jsonData['restaurants']
+    let dataList = '';
+    datas.forEach(function(data) {
+        dataList +=`
+        <div class="post-item">
+            <img class="post-item__thumb" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
+            <div class="city">${data['city']}</div>
+            <div class="post-item__content">
+                <p class="post-item__rating">
+                    Rating : 
+                    <a href="#" class="post_item__rating">${data['rating']}</a>
+                </p>
+                <h1 class="post-item__title"><a href="#">${data['name']}</a></h1>
+                <div class="post-item__description">${data['description'].slice(0, 150)}...</div>
+            </div>
+        </div>
+        `;
+    });
+    document.querySelector('#tes').innerHTML = dataList;  
+});
 
 const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
+const bodyElement = document.querySelector('body');
 
 menu.addEventListener('click', function (event) {
     drawer.classList.toggle('open');
